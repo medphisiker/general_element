575a5e043ce81877fb88244d0220aa2028394857
Anton Shiryaev <med.phisiker@gmail.com>
2025-08-09 22:02:25 +1000

    feat(test.py): добавил тесты
    - Массив с отрицательными числами
    - Все элементы одинаковые
    - Граничный случай: majority ровно n/2 + 1

--- test.py
+++ test.py
@@ -1,30 +1,47 @@
 from solution import find_general_elem
 
+
 def test():
     # Пример нечетная длинна массива элемент есть
-    array  = [7, 7, 8, 8, 8, 8, 9]
+    array = [7, 7, 8, 8, 8, 8, 9]
     right_answer = 8
     run_one_test(array, right_answer)
-    
+
     # Тест четная длинна массива элемент есть
-    array  = [7, 8, 8, 8, 8, 8]
+    array = [7, 8, 8, 8, 8, 8]
     right_answer = 8
     run_one_test(array, right_answer)
-    
+
     # Тест четная длинна массива, элемента нет
-    array  = [1, 2, 3, 4, 5, 6]
+    array = [1, 2, 3, 4, 5, 6]
     right_answer = -1
     run_one_test(array, right_answer)
-    
+
     # Тест массив длины в 1 элемент
-    array  = [1]
+    array = [1]
     right_answer = 1
     run_one_test(array, right_answer)
-    
+
     # Тест пустой массив
-    array  = []
+    array = []
     right_answer = -1
     run_one_test(array, right_answer)
+
+    # Массив с отрицательными числами
+    array = [-1, -1, -2, -2, -2]
+    right_answer = -2
+    run_one_test(array, right_answer)
+
+    # Все элементы одинаковые
+    array = [5, 5, 5, 5]
+    right_answer = 5
+    run_one_test(array, right_answer)
+
+    # Граничный случай: majority ровно n/2 + 1
+    array = [1, 1, 1, 2, 2]
+    right_answer = 1
+    run_one_test(array, right_answer)
+
 
 def run_one_test(array, right_answer):
     print()
